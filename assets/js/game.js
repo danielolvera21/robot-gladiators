3d2d62b5631df413806a29f69eecb12e8115409e
Daniel O <danielolvera2188@gmail.com>
2021-05-16 23:09:27 -0600

    variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! JavaScript is running!");
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+var playername = window.prompt("What is your robot's name?");
+
+console.log(playername);
+console.log("this logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10 + 10);
+//what is this?
+console.log("Our robots name is " + playername);
+
+//fight();
